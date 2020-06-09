079bb23d6b7b54936e34fde58f4aa1bc26e89361
Trent Smith <trentnsmith@gmail.com>
2020-06-08 23:31:11 -0500

    updaing api endpoint to production server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     API_KEY: 'AIzaSyDgnjyXg5rJiz9h1cPLo4ZMMFjL1nxkSr8',
-    API_ENDPOINT: 'http://localhost:8000'
+    API_ENDPOINT: 'https://evening-sea-86701.herokuapp.com/api'
 }
