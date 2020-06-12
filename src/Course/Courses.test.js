b48a97243b4459de32eecad2cab7a32f832be05b
Trent Smith <trentnsmith@gmail.com>
2020-06-12 18:56:47 -0500

    adjusting test files

--- src/Course/Courses.test.js
+++ src/Course/Courses.test.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import Courses from './Course';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  const props = {
+    id: '123',
+    name: 'test name',
+    holes: 'test holes',
+    city: 'test city',
+  }
   ReactDOM.render(
     <BrowserRouter>
-      <App />
+      <Courses {...props} />
     </BrowserRouter>,
     div
   );
--- src/ResultsList/ResultsList.test.js
+++ src/ResultsList/ResultsList.test.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import ResultsList from '././ResultsList';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    div
-  );
+  ReactDOM.render(<ResultsList />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
